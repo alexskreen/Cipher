5f4267ecb55a1639ca5f8fdb132dc2a937044422
Student <example@example.com>
2020-01-13 13:38:57 -0800

    add two functions to return characters and flip them

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,22 @@ $(document).ready(function() {
     var firstCharacter = sentence.charAt(0);
     var lastCharacter = sentence[sentence.length-1];
     alert(firstCharacter.toUpperCase() + lastCharacter.toUpperCase());
+    var sum1 = firstCharacter.toUpperCase() + lastCharacter.toUpperCase();
+    var sum2 = (lastCharacter.toUpperCase() + firstCharacter.toUpperCase());
+    alert(sum2)
+
+});
+
+// $(document).ready(function() {
+//     var sentence = prompt("Enter a sentence");
+//     var firstCharacter = sentence.charAt(0);
+//     var lastCharacter = sentence[sentence.length-1];
+//     alert(firstCharacter.toUpperCase() + lastCharacter.toUpperCase());
 
     
-    var jumble = function(){
+//     var jumble = function(){
     
-    };
-});
+//     };
+// });
+
+
