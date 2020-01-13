ecc5af8c8def9b34eac8a0dd2ae76c4b05e52bd3
Student <example@example.com>
2020-01-13 11:45:03 -0800

    add string method for first and last characters

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    var sentence = prompt("Enter a sentence");
+    var firstCharacter = sentence.charAt(0);
+    var lastCharacter = sentence[sentence.length-1];
+    alert(firstCharacter.toUpperCase() + lastCharacter.toUpperCase());
+
+    
+    var jumble = function(){
+    
+    };
+});
